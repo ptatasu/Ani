8585274b27257aa01a43f062dabe4ce560b365ab
ptatasu <garymason922@gmail.com>
2023-04-23 00:19:49 +0800

    removed the img tag
    replaced it with background-image in css

--- js/index.js
+++ js/index.js
@@ -14,22 +14,21 @@ $(document).ready(async () => {
         // names = info.otherName;
         const genre = genres.replaceAll(/[,"[\]]/g, (i) => chars[i]);
         // console.log(names);
+        const style = `width: 300px; height: 450px; background-image: url(${info.image}); background-size: cover; background-repeat: no-repeat;`;
         data = `<div class="genre">${genre}</div><div class="status">${info.status}</div>`;
         if (animeInfo.hasSub === true && animeInfo.hasDub === true) {
-            list += `<div data-names="${info.otherName}" class="item sub dub" id="${animeInfo.url.slice(7)}">`;
+            list += `<div data-names="${info.otherName}" class="item sub dub" id="${animeInfo.url.slice(7)}" style="${style}">`;
         } else if (animeInfo.hasDub === true) {
-            list += `<div data-names="${info.otherName}" class="item dub" id=" ${animeInfo.url.slice(7)}">`;
+            list += `<div data-names="${info.otherName}" class="item dub" id="${animeInfo.url.slice(7)}" style="${style}">`;
         } else if (animeInfo.hasSub === true) {
-            list += `<div data-names="${info.otherName}" class="item sub" id=" ${animeInfo.url.slice(7)}">`;
+            list += `<div data-names="${info.otherName}" class="item sub" id="${animeInfo.url.slice(7)}" style="${style}">`;
         }
-        list += `<img id="poster" data-names="${info.status}" src="${info.image}" alt="poster_image" width="225" height="311" draggable="false"/>
-                    <p class="title">${animeInfo.title}</p>            
+        list += `<p class="title">${animeInfo.title}</p>            
                     ${data}
                     <p class="episodes">${animeInfo.totalEpisodes}</p>
                 </div>`;
         $('.container').html(list);
         status = info.status;
-        // genresList = genre;
     });
     $('.container').on('click', '.item', function () {
         const episodeId = $(this).attr('id');
